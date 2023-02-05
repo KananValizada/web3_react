89ae4878ae6f160a48b139aaa66c8ed0a9362191
KananValizada <kenan.veli@yahoo.com>
2023-02-05 12:03:29 +0400

    sidebar

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      epilogue: ["Epilogue", "sans-serif"],
+    },
+    boxShadow: {
+      secondary: "10px 10px 20px rgba(2, 2, 2, 0.25)",
+    },
   },
   plugins: [],
 };
