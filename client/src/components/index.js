33273b275509406a89314583f57ee29bf3cafa46
KananValizada <kenan.veli@yahoo.com>
2023-02-07 23:30:05 +0400

    last part

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,4 +4,13 @@ import CustomButton from "./CustomButton";
 import FormField from "./FormField";
 import DisplayCampaigns from "./DisplayCampaigns";
 import FundCard from "./FundCard";
-export { Navbar, FormField, Sidebar, CustomButton, DisplayCampaigns, FundCard };
+import CountBox from "./CountBox";
+export {
+  Navbar,
+  FormField,
+  Sidebar,
+  CustomButton,
+  DisplayCampaigns,
+  FundCard,
+  CountBox,
+};
